67c913372dc05fd7ffe6897cb4953a474fc53740
abhaydalmia <dalmia.abhay@outlook.com>
2016-04-16 20:30:50 -0400

    Final Submission

--- finalIndexes.m
+++ finalIndexes.m
@@ -4,10 +4,10 @@ for i=1:length(companyNames)
     numberConform = [];
     for j=1:length(indexes)
         index = indexes(j);
-        mask = find(indexes> index-3 & indexes<index+3);
+        mask = find(indexes> index-5 & indexes<index+5);
         numberConform = [numberConform length(mask)];
     end
     [~,ind] = max(numberConform);
     finalIndex = indexes(ind(end));
-    final.(companyNames{i})=finalIndex;
+    final1.(companyNames{i})=finalIndex;
 end
